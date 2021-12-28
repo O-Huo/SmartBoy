d59cca3062437adec907dfb547a56be90ce0a0d1
Ao Li <leeleo3x@gmail.com>
2021-12-28 15:28:07 -0600

    update.

--- src/webhook.rs
+++ src/webhook.rs
@@ -4,24 +4,24 @@ mod telegram_client;
 mod user;
 use frankenstein::Api;
 use frankenstein::SetWebhookParams;
-use frankenstein::Update;
 use frankenstein::TelegramApi;
+use frankenstein::Update;
 use std::env;
 use std::net::Ipv4Addr;
 use tokio;
 use warp::{http, Filter};
 
 async fn process_telegram_updates(
-    updates: Vec<Update>
-    ) -> Result<impl warp::Reply, warp::Rejection> {
-        println!("updates received");
-        for update in updates {
-            // println!(update.message.)
-        }
-        Ok(warp::reply::with_status(
-            "Added items to the grocery list",
-            http::StatusCode::CREATED,
-        ))
+    updates: Vec<Update>,
+) -> Result<impl warp::Reply, warp::Rejection> {
+    println!("updates received");
+    for update in updates {
+        // println!(update.message.)
+    }
+    Ok(warp::reply::with_status(
+        "Added items to the grocery list",
+        http::StatusCode::CREATED,
+    ))
 }
 
 #[tokio::main]
@@ -37,14 +37,15 @@ async fn main() {
         allowed_updates: None,
         drop_pending_updates: None,
     };
-    api.set_webhook(&webhook_params).expect("Set webhook failed");
+    api.set_webhook(&webhook_params)
+        .expect("Set webhook failed");
 
     let tg_handler = warp::post()
-    .and(warp::path("api"))
-    .and(warp::path("tg-update"))
-    .and(warp::path::end())
-    .and(warp::body::json())
-    .and_then(process_telegram_updates);
+        .and(warp::path("api"))
+        .and(warp::path("tg-update"))
+        .and(warp::path::end())
+        .and(warp::body::json())
+        .and_then(process_telegram_updates);
 
     let port = 3000;
 
